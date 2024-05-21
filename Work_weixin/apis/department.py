f2f2ad5fb6538ee36c550235c3dd534f1695627f
aliyun8981839108 <>
2024-05-21 10:29:19 +0800

    员工管理

--- Work_weixin/apis/department.py
+++ Work_weixin/apis/department.py
@@ -8,6 +8,7 @@ from Work_weixin.apis.wework import WeWork
 # 部门需要继承 Wework，这样就可以直接获取 Wework 中获取到的 access_token
 
 class Department(WeWork):
+    #封装get_token，避免多次调用这个敏感的接口
     def __init__(self):
         super().__init__()
         self.gettoken = self.get_token()
@@ -44,3 +45,13 @@ class Department(WeWork):
         get_url = f'{self.base_url}/cgi-bin/department/simplelist?access_token={self.gettoken}'
         r = requests.get(get_url)
         return r
+
+    def creat_worker(self,data):
+        creatworker_url = f'{self.base_url}/cgi-bin/user/create?access_token={self.get_token()}'
+        r = requests.post(creatworker_url,json=data)
+        return r
+
+    def del_worker(self,param):
+        del_url = f'{self.base_url}/cgi-bin/user/delete?access_token={self.get_token()}'
+        r = requests.get(del_url,params=param)
+        return  r
--- Work_weixin/apis/wework.py
+++ Work_weixin/apis/wework.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
+
+from Work_weixin.apis.base_api import BaseApi
+
 '''
     封装业务的全局操作
     企业ID
@@ -7,12 +10,16 @@ import requests
     获取token
     '''
 
-class WeWork:
+class WeWork(BaseApi):
     def __init__(self):
         self.base_url = 'https://qyapi.weixin.qq.com'
         self.secreat = 'NBh_33dSqN4CMV4PLw2x2wsBuWNyUfOyUS_A-hJHy1I'
         self.scorpid = 'wwfc559d7de1d0d9c8'
         self.token = self.get_token()
+
+        req = {
+
+        }
 
     def get_token(self):
         param = {
